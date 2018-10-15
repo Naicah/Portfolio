b1bb89f87a02b8e40053d5429c848b4ac1b94c34
nina hedman <ni.hedman@gmail.com>
2018-10-15 16:30:55 +0200

    Trying to change activeNav on scroll

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,11 +1,25 @@
+var home = document.getElementById("home");
+var about = document.getElementById("about");
+var portfolio = document.getElementById("portfolio");
+var contact = document.getElementById("contact");
+
+var offset_home = home.offsetTop;
+var offset_about = about.offsetTop;
+var offset_portfolio = portfolio.offsetTop;
+var offset_contact = contact.offsetTop;
+
 var nav_about = document.getElementById("nav_about");
 var nav_portfolio = document.getElementById("nav_portfolio");
 var nav_contact = document.getElementById("nav_contact");
-var home = document.getElementById("home");
+
+
+
 var active_nav_pre = home;
-var active_nav_new;
+var active_nav_
 
-
+/*------------------------------------------------------------
+  -                        NAV                               -
+  ------------------------------------------------------------ */
 function activeNav(nav_new) {
     if (active_nav_pre != home) { //Don't toggle 'active' on home, only elements in nav
         active_nav_pre.classList.toggle("active");
@@ -14,8 +28,7 @@ function activeNav(nav_new) {
     active_nav_pre = nav_new;
 }
 
-
-
+//Klick på sektioner i nav
 document.getElementById('nav_about').addEventListener('click', function () {
     active_nav_new = nav_about;
     activeNav(nav_about);
@@ -30,12 +43,41 @@ document.getElementById('nav_contact').addEventListener('click', function () {
 })
 
 
-/*
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+function changeActivNavOnScroll() {
+    if (window.pageYOffset >= offset_home) {
+       console.log("Toppen av home");
+       console.log(offset_home)
+    } else if (window.pageYOffset = offset_about) {
+        console.log("Toppen av about");
+        console.log(offset_about)
+        active_nav_new = nav_about;
+        activeNav(nav_about);
+    } else if (window.pageYOffset = offset_portfolio) {
+        console.log("Toppen av portfolio");
+        console.log(offset_portfolio)
+        active_nav_new = nav_portfolio;
+        activeNav(nav_portfolio);
+    }  else if (window.pageYOffset = offset_contact) {
+        console.log(offset_contact)
+        console.log("Toppen av contact");
+        active_nav_new = nav_contact;
+        activeNav(nav_contact);
+    }
+}
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+
+// When the user scrolls the page
+window.onscroll = function() {
+    console.log("Scrolling: " + window.pageYOffset)
+    changeActivNavOnScroll()
+
+};
+
+
+
+
+/*
+
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;

4b6bd5609b77b917f692dc6d38451c9fb6b7d802
nina hedman <ni.hedman@gmail.com>
2018-10-15 15:47:40 +0200

    Sticky nav, space in shell

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -29,3 +29,23 @@ document.getElementById('nav_contact').addEventListener('click', function () {
     activeNav(nav_contact);
 })
 
+
+/*
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+*/

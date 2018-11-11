e08d21ce66072a7f378b73bcea83ae03423cefab
nina hedman <ni.hedman@gmail.com>
2018-11-11 12:46:00 +0100

    Fixed highlight nav + cleanup

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -3,33 +3,22 @@ var about = document.getElementById("about");
 var portfolio = document.getElementById("portfolio");
 var contact = document.getElementById("contact");
 
-var element_height;
 var element_space;
-
 var screen_height = window.innerHeight;
 var screen_width = window.innerWidth;
 
 var scrollTop_win_now; //Top position rigth now
-var scrollTop_win_before;
+var scrollTop_win_before; // Previos top position (when scrolling)
 
-var pos_body = document.body.getBoundingClientRect();
-   
-
-var pos_home = home.getBoundingClientRect();
-var top_home = pos_home.top - pos_body.top;
-var bottom_home = pos_home.bottom;
-
-var pos_about = about.getBoundingClientRect();
-var top_about = pos_about.top - pos_body.top;
-var bottom_about = pos_about.bottom;
-
-var pos_portfolio = portfolio.getBoundingClientRect();
-var top_portfolio = pos_portfolio.top - pos_body.top;
-var bottom_portfolio = pos_portfolio.bottom;
-
-var pos_contact = contact.getBoundingClientRect();
-var top_contact = pos_contact.top - pos_body.top;
-var bottom_contact = pos_contact.bottom;
+var pos_body;
+var pos_home;
+var top_home;
+var pos_about;
+var top_about;
+var pos_portfolio;
+var top_portfolio;
+var pos_contact;
+var top_contact;
 
 var nav_bg =  document.getElementById("nav_bg");
 var logo = document.getElementById("logo");
@@ -37,44 +26,52 @@ var nav_home = document.getElementById("nav-home");
 var nav_about = document.getElementById("nav-about");
 var nav_portfolio = document.getElementById("nav-portfolio");
 var nav_contact = document.getElementById("nav-contact");
-
 var active_nav_pre = nav_home;
+
 var one_pro_height = screen_height/100;
 var new_height;
 var min_height = one_pro_height*10; //10% of screen height
 var max_height = one_pro_height*81; //81% of screen height
-   // console.log("1%: " + one_pro_height);
-   // console.log("10%: " + min_height);
-   // console.log("80%: " + max_height);
-   // console.log("Screen height " + screen_height); 
 
+/*------------------------------------------------------------
+  -                        ON LOAD                           -
+  ------------------------------------------------------------ */
 
 window.onload = function() {
-    console.log("onload");
     scrollTop_win_now = document.documentElement.scrollTop; //Find top position
-    console.log("onload scrolltop: " + scrollTop_win_now);
+    getSectionPosition();
     changeActivNavOnScroll(); // Change what section in nav that is highlighted
     changeHeight(logo, logo.offsetHeight);
 };
 
+/*------------------------------------------------------------
+  -                        MAIN                              -
+  ------------------------------------------------------------ */
+function getSectionPosition () {
+    pos_body = document.body.getBoundingClientRect();
+    pos_home = home.getBoundingClientRect();
+    top_home = pos_home.top - (pos_body.top);
+    pos_about = about.getBoundingClientRect();
+    top_about = pos_about.top - (pos_body.top);
+    pos_portfolio = portfolio.getBoundingClientRect();
+    top_portfolio = pos_portfolio.top - (pos_body.top);
+    pos_contact = contact.getBoundingClientRect();
+    top_contact = pos_contact.top - (pos_body.top);
+}
 
 /*------------------------------------------------------------
   -                        NAV                               -
   ------------------------------------------------------------ */
 
-function changeHeight(element, element_height) {
-   // console.log("Element: " + element_height);
+function changeHeight(element, element_height) { //Change height of given element
     if ((element_height > min_height) && (element_height < max_height)) {
         element_space = screen_height - scrollTop_win_now;
-     //   console.log("Logo space: " + element_space);
         element_height = (element_space/10) * 8;
         if (element_height > min_height) {
-     //       console.log("New element height: " + element_height);
             element.style.height = element_height + 'px';
             moveElementDiagonal_MiddleTop(logo, element_height);
         }
     }
-   
 }
 
 function moveElementDiagonal_MiddleTop(element, element_height) {
@@ -95,64 +92,54 @@ function activeNav(active_nav_new) {
     active_nav_pre = active_nav_new;
 }
 
-//When click on section in nav
-nav_about.addEventListener('click', function () {
-    activeNav(nav_about);
-})
-nav_portfolio.addEventListener('click', function () {
-   
-    scrollTop_win_before = scrollTop_win_now;
-    scrollTop_win_now =  document.documentElement.scrollTop; //Find top position
-    activeNav(nav_portfolio);
-    console.log("top portfolio: "+ top_portfolio);
-    window.scrollTo(0, top_portfolio);
-})
-nav_contact.addEventListener('click', function () {
-    activeNav(nav_contact);
-})
-
 //Check where top position is and change highlight in nav accordingly
 function changeActivNavOnScroll() {
-    console.log("scrolltop: " + scrollTop_win_now);
      if (scrollTop_win_now >= top_contact) { //Within contact section
         activeNav(nav_contact);
-      //  nav_bg.classList.add("fade");
-    //  console.log("Contact top: " + scrollTop_contact);
-    //  console.log("Contact bottom: " + scrollBottom_contact);
     } else if (scrollTop_win_now >= top_portfolio) { //Within portfolio section
         activeNav(nav_portfolio);
-      //  nav_bg.classList.add("fade");
-    //  console.log("Portfolio top: " + scrollTop_portfolio);
-    //  console.log("Portfolio bottom: " + scrollBottom_portfolio);
     }  else if (scrollTop_win_now >= top_about){ //Within about section
         activeNav(nav_about);
         nav_bg.classList.remove("fadeOut");
         nav_bg.classList.add("fadeIn");
-     //   console.log("About top: " + scrollTop_about);
-    //    console.log("About bottom: " + scrollBottom_about);
     } else { //Within home section
         activeNav(nav_home);
         nav_bg.classList.remove("fadeIn");
         nav_bg.classList.add("fadeOut");
         nav_home.classList.remove("active");
-     //   console.log("Home top: " + scrollTop_home);
-     //   console.log("Home bottom: " + scrollBottom_home);
     }
 }
 
-
 // When the user scrolls the page
 window.onscroll = function() {
+    getSectionPosition();
     scrollTop_win_before = scrollTop_win_now;
     scrollTop_win_now = document.documentElement.scrollTop; //Find top position
     changeActivNavOnScroll(); // Change what section in nav that is highlighted
-    changeHeight(logo, logo.offsetHeight);
-    console.log("pos portfolio: "+ pos_portfolio);
-    console.log("top portfolio: "+ top_portfolio);
-    
-
+    changeHeight(logo, logo.offsetHeight); // Change height of logo
 };
 
+//When click on section in nav
+logo.addEventListener('click', function () {
+    getSectionPosition();
+    window.scrollTo(0, (top_home + 10));
+    activeNav(nav_home);
+})
+nav_about.addEventListener('click', function () {
+    getSectionPosition();
+    window.scrollTo(0, (top_about + 10));
+    activeNav(nav_about);
+})
+nav_portfolio.addEventListener('click', function () {
+    getSectionPosition();
+    window.scrollTo(0, (top_portfolio + 10));
+    activeNav(nav_portfolio);
+})
+nav_contact.addEventListener('click', function () {
+    getSectionPosition();
+    window.scrollTo(0, (top_contact + 10));
+    activeNav(nav_contact);
+})
 
 
 /*------------------------------------------------------------
